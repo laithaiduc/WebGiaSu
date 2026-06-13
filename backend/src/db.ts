9ae12d22c5e064db4788f440c5dc26431a6f949e
laithaiduc <haytrao12@gmail.com>
2026-06-13 10:47:39 +0700

    Change MySQL connection details to Aiven settings
    Updated MySQL connection settings for Aiven cloud.

--- backend/src/db.ts
+++ backend/src/db.ts
@@ -1,11 +1,11 @@
 import mysql from 'mysql2/promise';
 
 const {
-  MYSQL_HOST = 'localhost',
-  MYSQL_PORT = '3307',
-  MYSQL_USER = 'root',
-  MYSQL_PASSWORD = '',
-  MYSQL_DATABASE = 'webgiasu',
+  MYSQL_HOST = 'mysql-1732200e-haytrao12-2d67.e.aivencloud.com',
+  MYSQL_PORT = '25240',
+  MYSQL_USER = 'avnadmin',
+  MYSQL_PASSWORD = 'MAT_KHAU_TREN_AIVEN_CUA_ONG', // Bật mắt 👁️ bên Aiven để copy mật khẩu thật vào đây
+  MYSQL_DATABASE = 'defaultdb',
 } = process.env;
 
 const pool = mysql.createPool({
@@ -14,6 +14,9 @@ const pool = mysql.createPool({
   user: MYSQL_USER,
   password: MYSQL_PASSWORD,
   database: MYSQL_DATABASE,
+  ssl: {
+    rejectUnauthorized: false, // Bắt buộc phải thêm dòng này để bypass SSL của Aiven khi kết nối từ xa
+  },
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
